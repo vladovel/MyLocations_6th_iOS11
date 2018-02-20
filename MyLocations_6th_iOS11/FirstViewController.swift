e3cbc31cce4b8b54e345001b13132de22e3f22c4
vladovel <vladovel@gmail.com>
2018-02-20 12:50:59 +0100

    Initial Commit

---
+++ MyLocations_6th_iOS11/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MyLocations_6th_iOS11
+//
+//  Created by Vlado Velkovski on 2/20/18.
+//  Copyright © 2018 Vlado Velkovski. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
